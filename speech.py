c5af26281955db2bd5b72e469c693d1a7c536646
Oleg Naumov <oleg.naumov.98@mail.ru>
2019-01-23 23:31:27 +0300

    reply on audio msg with recognized speech

--- speech.py
+++ speech.py
@@ -3,14 +3,14 @@ import cloudconvert
 import urllib.request
 import requests
 import time
+import random
 
-# 2fd35efed90bc11ff20542d3d42164e75e454a83
-# imv4.vk.com\/im3491
-# 1841778296
+# scope = friends,photos,audio,video,status,messages,wall,docs,groups,offline
+
 def get_vk_token():
     return open('vk_api_token.txt').read()
 
-def convert_ogg_to_wav(ogg_audio):
+def convert_ogg_to_wav(ogg_audio_file):
     api_key_file = 'api_key.txt'
     api_key = open(api_key_file).read()
     api = cloudconvert.Api(api_key)
@@ -19,16 +19,16 @@ def convert_ogg_to_wav(ogg_audio):
         "inputformat": "ogg",
         "outputformat": "wav",
         "input": "upload",
-        "file": open(ogg_audio, 'rb')
+        "file": open(ogg_audio_file, 'rb')
     })
 
     process.wait()
     process.download()
  
 
-def download_audio(url, wav_audio):
+def download_audio(url, audio_name_ogg):
     audio_msg = urllib.request.urlopen(url).read()
-    f = open(wav_audio, "wb")
+    f = open(audio_name_ogg, "wb")
     f.write(audio_msg)
     f.close()
 
@@ -48,21 +48,22 @@ if __name__ == "__main__":
     url = 'https://psv4.userapi.com/c852428//u143857006/audiomsg/d15/0b95a179cd.ogg'
     ogg_voice = 'voice.ogg'
     wav_voice = 'voice.wav'
-    # convert_ogg_to_wav(ogg_voice)
-    # print(recognize(wav_voice))
-
+    vk_token = get_vk_token()
     long_poll_server = requests.get('https://api.vk.com/method/messages.getLongPollServer',
-                        params={'access_token': get_vk_token(), 'v': 5.92}).json()['response']
-
+                        params={'access_token': vk_token, 'v': 5.92}).json()['response']
+    print(long_poll_server)
     while True:
+        # отправление запроса на Long Poll сервер со временем ожидания 20 и опциями ответа 2
         response = requests.get('https://{server}?act=a_check&key={key}&ts={ts}&wait=20&mode=2&version=2'.format(
-                server=long_poll_server['server'], key=long_poll_server['key'], ts=long_poll_server['ts'])).json()  # отправление запроса на Long Poll сервер со временем ожидания 20 и опциями ответа 2
+                server=long_poll_server['server'], key=long_poll_server['key'], ts=long_poll_server['ts'])).json()
         try:
             updates = response['updates']
-        except KeyError:  # если в этом месте возбуждается исключение KeyError, значит параметр key устарел, и нужно получить новый
+        # если в этом месте возбуждается исключение KeyError, значит параметр key устарел, и нужно получить новый
+        except KeyError:
+            # получение ответа от сервера
             long_poll_server = requests.get('https://api.vk.com/method/messages.getLongPollServer',
-                                            params={'access_token': get_vk_token()}).json()['response']  # получение ответа от сервера
-            continue  # переходим на следующую итерацию цикла, чтобы сделать повторный запрос
+                                            params={'access_token': vk_token}).json()['response']
+            continue
         if updates:
             for element in updates:
                 summands = []  # массив, где мы будем хранить слагаемые
@@ -74,55 +75,32 @@ if __name__ == "__main__":
                 if 2 not in summands:
                     if element[0] == 4:
                         print(element)
-                        if element[3] - 2000000000 > 0:
-                            user_id = element[6]['from']  # id отправителя
-                            chat_id = element[3] - 2000000000  # id беседы
-                            chat = requests.get('https://api.vk.com/method/messages.getChat',
-                                                params={'chat_id': chat_id, 'access_token': get_vk_token()}).json()['response']['title']  # получение названия беседы
-                            # user = requests.get('https://api.vk.com/method/users.get',
-                            #                     params={'user_ids': user_id, 'name_case': 'gen'}).json()['response'][0]  # получение имени и фамилии пользователя, отправившего сообщение
-                            time_ = element[4]  # время отправления сообщения
-                            text = element[5]  # текст сообщения
-                            if text:  # проверяем, что сообщение содержит текст
-                                print(time.ctime(time_).split()[3], ':', 'Сообщение от', user_id, 'в беседе "{}"'.format(chat) + ':', text)
-                        else:
-                            user_id = element[3]  # id собеседника
-                            # user = requests.get('https://api.vk.com/method/users.get',
-                            #                     params={'user_ids': user_id, 'name_case': 'gen'}).json()['response'][0]  # получение имени и фамилии пользователя, отправившего сообщение
-                            time_ = element[4]  # время отправления сообщения
-                            text = element[5]  # текст сообщения
-                            if text:  # проверяем, что сообщение содержит текст
-                                print(time.ctime(time_).split()[3], ':', 'Сообщение от', user_id, ':', text)
-
-
                         index = 1
-                        photos = []  # массив для хранения id фотографий
-                        docs = []  # массив для хранения id документов
                         media_type = 'attach1_type'
+                        msg_element = {}
                         while media_type in element[6].keys():  # проверка, существует ли медиа-вложение с таким индексом
-                            media_type = element[6]['attach{}_type'.format(index)]  # если существует, сохраняем его тип
-                            if media_type == 'photo':  # является ли вложение фотографией
-                                photos.append(element[6]['attach{}'.format(index)])  # добавляем id фотографии в массив
-                            elif media_type == 'doc':  # является ли вложение документом
-                                docs.append(element[6]['attach{}'.format(index)])  # добавляем id документа в массив
+                            media_type = element[6]['attach{}_type'.format(index)]
+                            attach_kind = 'attach{}_kind'.format(index)
+                            if media_type == 'doc' and attach_kind in element[6].keys():  # является ли вложение документом
+                                if element[6][attach_kind] == 'audiomsg':
+                                    msg_element = dict(msg_id=element[1], peer_id=element[3], doc=element[6]['attach{}'.format(index)])
+                                    break
                             index += 1  # увеличиваем индекс
                             media_type = 'attach{}_type'.format(index)
-                        change = lambda ids, type_: requests.get(
-                            'https://api.vk.com/method/{}.getById'.format(type_),
-                            params={type_: ids, 'access_token': get_vk_token(), 'v': 5.92}
-                        ).json()  # функция, возвращающаяся ссылки на объекты
-                        if photos:  # проверка, были ли во вложениях фотографии
-                            photos = change(', '.join(photos), 'photos')  # если были, то перезаписываем переменную photos на словарь
-                            if 'response' in photos.keys():
-                                photos = [attachment['src_xbig'] for attachment in photos['response']]  # перезаписываем на ссылки
-                                print('сообщение содержит следующие фотографии:', ', '.join(photos))
-                            else:
-                                pass  # скорее всего, возникла ошибка доступа
-                        if docs:  # проверка, были ли во вложениях документы
-                            docs = change(', '.join(docs), 'docs')  # если были, то перезаписываем переменную docs на словарь
-                            if 'response' in docs.keys():
-                                docs = [attachment['url'] for attachment in docs['response']]  # перезаписываем на ссылки
-                                print('сообщение содержит следующие документы:', ', '.join(docs))
+
+                        if msg_element:
+                            msg_element['doc'] = requests.get('https://api.vk.com/method/docs.getById',
+                                                params={'docs': msg_element['doc'], 'access_token': vk_token, 'v': 5.92}).json()
+                            if 'response' in msg_element['doc'].keys():
+                                doc_url = msg_element['doc']['response'][0]['url']
+                                download_audio(doc_url, ogg_voice)
+                                convert_ogg_to_wav(ogg_voice)
+                                audio_text = recognize(wav_voice)
+                                print('Расшифровка голосового сообщения:', audio_text)
+
+                                send_msg = requests.get('https://api.vk.com/method/messages.send',
+                                             params={'access_token': vk_token, 'message': audio_text, 'random_id': random.randint(0, 10e9), 'user_id': msg_element['peer_id'], 'reply_to': msg_element['msg_id'], 'v': 5.92}).json()
+                                print(send_msg)
                             else:
                                 pass  # скорее всего, возникла ошибка доступа
 
