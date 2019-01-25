f588268a7c678f0f03c0a18ace8fd32a317ef2dc
Oleg Naumov <oleg.naumov.98@mail.ru>
2019-01-25 13:54:00 +0300

    Added logger

--- speech.py
+++ speech.py
@@ -3,11 +3,15 @@ import cloudconvert
 import urllib.request
 import requests
 import random
+import logging
 
 # scope = friends,photos,audio,video,status,messages,wall,docs,groups,offline
+logging.basicConfig(format=u'%(filename)s[LINE:%(lineno)d]# %(levelname)-8s %(message)s', level=logging.INFO,
+                    filename=u'speech.log', filemode='w')
 
 def get_vk_token():
     return open('vk_api_token.txt').read()
+
 
 def convert_ogg_to_wav(ogg_audio_file):
     api_key_file = 'api_key.txt'
@@ -23,6 +27,7 @@ def convert_ogg_to_wav(ogg_audio_file):
 
     process.wait()
     process.download()
+
 
 def download_audio(url, audio_name_ogg):
     audio_msg = urllib.request.urlopen(url).read()
@@ -42,8 +47,9 @@ def recognize(wav_audio):
             text = r.recognize_google(audio, language='ru-RU')
         except sr.UnknownValueError:
             text = "Речь неразборчива. Не могу распознать."
-        except sr.RequestError:
+        except sr.RequestError as reqE:
             text = "Распознавание завершилось неудачно."
+            logging.error(u'%s %s' % (text, reqE))
     return text
 
 
@@ -68,13 +74,13 @@ if __name__ == "__main__":
             continue
         if updates:
             for element in updates:
-                print(element)
+                logging.info(u'Событие %s' % (element))
                 summands = []  # массив, где мы будем хранить слагаемые
                 flag = element[2]  # флаг сообщения
                 for number in [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 65536]:  # проходим циклом по возможным слагаемым
                     if flag & number:  # проверяем, является ли число слагаемым с помощью побитового И
                         summands.append(number)  # если является, добавляем его в массив
-
+                logging.info(u'Флаги события %s' % (flag))
                 if 2 not in summands:
                     if element[0] == 4:
                         index = 1
@@ -84,17 +90,20 @@ if __name__ == "__main__":
                             media_type = element[6]['attach{}_type'.format(index)]
                             attach_kind = 'attach{}_kind'.format(index)
                             if media_type == 'doc' and attach_kind in element[6].keys():  # является ли вложение документом
-                                if element[6][attach_kind] == 'audiomsg':   # является ли вложение аудио-сообщением
+                                if element[6][attach_kind] == 'audiomsg':  # является ли вложение аудио-сообщением
                                     msg_id = element[1]
                                     break
                             index += 1
                             media_type = 'attach{}_type'.format(index)
+
+                        logging.info(u'Входящее сообщение с номером %s' % (msg_id))
                         if msg_id != None:
                             message = requests.get('https://api.vk.com/method/messages.getById',
                                                    params={'access_token': vk_token, 'message_ids': [msg_id], 'v': 5.92}).json()
 
                             if 'response' in message.keys():
                                 ogg_link = message['response']['items'][0]['attachments'][0]['audio_message']['link_ogg']
+                                logging.info(u'Ссылка на аудио сообщение %s' % (ogg_link))
                                 from_id = message['response']['items'][0]['from_id']
                                 download_audio(ogg_link, ogg_voice)
                                 convert_ogg_to_wav(ogg_voice)
@@ -107,6 +116,5 @@ if __name__ == "__main__":
                                                                 'random_id': random.randint(0, 10e9),
                                                                 'user_id': from_id,
                                                                 'reply_to': msg_id, 'v': 5.92}).json()
-                                print(send_msg)
 
         long_poll_server['ts'] = response['ts']
