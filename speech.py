af75a64944a2f14159449c627625706eff53ba66
Oleg Naumov <oleg.naumov.98@mail.ru>
2019-01-25 21:04:51 +0300

    Reworked converting from ogg to wav with ffmpeg utility"

--- speech.py
+++ speech.py
@@ -1,9 +1,9 @@
 import speech_recognition as sr
-import cloudconvert
 import urllib.request
 import requests
 import random
 import logging
+import subprocess
 
 # scope = friends,photos,audio,video,status,messages,wall,docs,groups,offline
 logging.basicConfig(format=u'%(filename)s[LINE:%(lineno)d]# %(levelname)-8s %(message)s', level=logging.INFO,
@@ -13,20 +13,8 @@ def get_vk_token():
     return open('vk_api_token.txt').read()
 
 
-def convert_ogg_to_wav(ogg_audio_file):
-    api_key_file = 'api_key.txt'
-    api_key = open(api_key_file).read()
-    api = cloudconvert.Api(api_key)
-
-    process = api.convert({
-        "inputformat": "ogg",
-        "outputformat": "wav",
-        "input": "upload",
-        "file": open(ogg_audio_file, 'rb')
-    })
-
-    process.wait()
-    process.download()
+def convert_ogg_to_wav(ogg_audio_file, wav_audio_file):
+    subprocess.run("ffmpeg -y -i {ogg} {wav}".format(ogg=ogg_audio_file, wav=wav_audio_file).split())
 
 
 def download_audio(url, audio_name_ogg):
@@ -59,7 +47,6 @@ if __name__ == "__main__":
     vk_token = get_vk_token()
     long_poll_server = requests.get('https://api.vk.com/method/messages.getLongPollServer',
                                     params={'access_token': vk_token, 'v': 5.92}).json()['response']
-    print(long_poll_server)
     while True:
         # отправление запроса на Long Poll сервер со временем ожидания 20 и опциями ответа 2
         response = requests.get('https://{server}?act=a_check&key={key}&ts={ts}&wait=20&mode=2&version=2'.format(
@@ -110,7 +97,7 @@ if __name__ == "__main__":
                                 logging.info(u'Ссылка на аудио сообщение %s' % (ogg_link))
                                 from_id = message['response']['items'][0]['from_id']
                                 download_audio(ogg_link, ogg_voice)
-                                convert_ogg_to_wav(ogg_voice)
+                                convert_ogg_to_wav(ogg_voice, wav_voice)
                                 audio_text = recognize(wav_voice)
                                 print('Расшифровка голосового сообщения:', audio_text)
 
