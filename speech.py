2a1ae31718e11f43d6402c0c88e88e340e8d7d3e
Oleg Naumov <oleg.naumov.98@mail.ru>
2019-01-25 22:45:30 +0300

    Rewrited with vk_api module

--- speech.py
+++ speech.py
@@ -1,9 +1,10 @@
 import speech_recognition as sr
 import urllib.request
-import requests
 import random
 import logging
 import subprocess
+import vk_api
+from vk_api.longpoll import VkLongPoll, VkEventType
 
 # scope = friends,photos,audio,video,status,messages,wall,docs,groups,offline
 logging.basicConfig(format=u'%(filename)s[LINE:%(lineno)d]# %(levelname)-8s %(message)s', level=logging.INFO,
@@ -44,68 +45,31 @@ def recognize(wav_audio):
 if __name__ == "__main__":
     ogg_voice = 'voice.ogg'
     wav_voice = 'voice.wav'
-    vk_token = get_vk_token()
-    long_poll_server = requests.get('https://api.vk.com/method/messages.getLongPollServer',
-                                    params={'access_token': vk_token, 'v': 5.92}).json()['response']
-    while True:
-        # отправление запроса на Long Poll сервер со временем ожидания 20 и опциями ответа 2
-        response = requests.get('https://{server}?act=a_check&key={key}&ts={ts}&wait=20&mode=2&version=2'.format(
-            server=long_poll_server['server'], key=long_poll_server['key'], ts=long_poll_server['ts'])).json()
-        try:
-            updates = response['updates']
-        # если в этом месте возбуждается исключение KeyError, значит параметр key устарел, и нужно получить новый
-        except KeyError:
-            # получение ответа от сервера
-            long_poll_server = requests.get('https://api.vk.com/method/messages.getLongPollServer',
-                                            params={'access_token': vk_token}).json()['response']
-            continue
-        if updates:
-            for element in updates:
-                logging.info(u'Событие %s' % (element))
-                summands = []  # массив, где мы будем хранить слагаемые
-                flag = element[2]  # флаг сообщения
-                # Иногда проскакиывают события, не имеющие флагов, поэтому их не обрабатываем
+
+    vk_session = vk_api.VkApi(token=get_vk_token())
+    vk = vk_session.get_api()
+    long_poll_server = VkLongPoll(vk_session)
+
+    for event in long_poll_server.listen():
+        if event.type == VkEventType.MESSAGE_NEW:
+            # если входящее сообщение и содержит вложения
+            if event.to_me and event.attachments:
+                attachments = event.attachments
+                logging.info(u'Входящее сообщение с вложениями: %s' % attachments)
+                # на случай, если вложение не является аудио сообщением
                 try:
-                    for number in [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 65536]:  # проходим циклом по возможным слагаемым
-                        if flag & number:  # проверяем, является ли число слагаемым с помощью побитового И
-                            summands.append(number)  # если является, добавляем его в массив
-                except TypeError:
+                    if attachments['attach1_kind'] == 'audiomsg':
+                        message = vk.messages.getById(message_ids=event.message_id, v=5.92)
+                        ogg_link = message['items'][0]['attachments'][0]['audio_message']['link_ogg']
+                        from_id = message['items'][0]['from_id']
+                        logging.info(u'Аудио сообщение от %d: %s' % (from_id, ogg_link))
+
+                        download_audio(ogg_link, ogg_voice)
+                        convert_ogg_to_wav(ogg_voice, wav_voice)
+                        audio_text = recognize(wav_voice)
+                        logging.info(u'Текст сообщения: %s' % audio_text)
+
+                        vk.messages.send(message=audio_text, peer_id=from_id, reply_to=event.message_id,
+                                         random_id=random.randint(0, 10e9), v=5.92)
+                except KeyError:
                     continue
-                logging.info(u'Флаги события %s' % (flag))
-                if 2 not in summands:
-                    if element[0] == 4:
-                        index = 1
-                        media_type = 'attach1_type'
-                        msg_id = None
-                        while media_type in element[6].keys():  # проверка, существует ли медиа-вложение с таким индексом
-                            media_type = element[6]['attach{}_type'.format(index)]
-                            attach_kind = 'attach{}_kind'.format(index)
-                            if media_type == 'doc' and attach_kind in element[6].keys():  # является ли вложение документом
-                                if element[6][attach_kind] == 'audiomsg':  # является ли вложение аудио-сообщением
-                                    msg_id = element[1]
-                                    break
-                            index += 1
-                            media_type = 'attach{}_type'.format(index)
-
-                        logging.info(u'Входящее сообщение с номером %s' % (msg_id))
-                        if msg_id != None:
-                            message = requests.get('https://api.vk.com/method/messages.getById',
-                                                   params={'access_token': vk_token, 'message_ids': [msg_id], 'v': 5.92}).json()
-
-                            if 'response' in message.keys():
-                                ogg_link = message['response']['items'][0]['attachments'][0]['audio_message']['link_ogg']
-                                logging.info(u'Ссылка на аудио сообщение %s' % (ogg_link))
-                                from_id = message['response']['items'][0]['from_id']
-                                download_audio(ogg_link, ogg_voice)
-                                convert_ogg_to_wav(ogg_voice, wav_voice)
-                                audio_text = recognize(wav_voice)
-                                print('Расшифровка голосового сообщения:', audio_text)
-
-                                send_msg = requests.get('https://api.vk.com/method/messages.send',
-                                                        params={'access_token': vk_token,
-                                                                'message': audio_text,
-                                                                'random_id': random.randint(0, 10e9),
-                                                                'user_id': from_id,
-                                                                'reply_to': msg_id, 'v': 5.92}).json()
-
-        long_poll_server['ts'] = response['ts']
